466e8dbc931848a5e2477bd32d4c62f8898ce207
finnformica <finnformica@gmail.com>
2023-01-15 23:54:12 +0000

    minor cosmetic changes

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -5,11 +5,11 @@ const Header = () => {
   const router = useRouter();
   return (
     <nav className="flex w-full border-b border-white/10">
-      <div className="flex p-8 md:w-2/3 w-1/2 box-border items-center">
-        <h1 className="text-3xl">luna goodwin</h1>
+      <div className="flex p-8 md:w-2/3 w-1/2 box-border items-center border-r border-white/10">
+        <h1 className="text-3xl">luna ocean</h1>
       </div>
 
-      <div className="flex md:w-1/3 w-1/2 justify-center items-center border-l border-white/10 gap-10">
+      <div className="flex md:w-1/3 w-1/2 justify-center items-center gap-10 md:gap-14 xl:gap-36">
         <Link
           href="/"
           style={{

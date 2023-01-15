3fbcc777ed4887b8dcebbea41ac06184ea566cd4
finnformica <finnformica@gmail.com>
2023-01-15 23:55:21 +0000

    add portfolio image to about page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,19 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+const DisplayImage = ({ name }) => {
+  return (
+    <img
+      className="object-cover w-full h-full shadow-[0_0_60px_60px_rgba(0,0,0,0.8)_inset]"
+      src={`/images/${name}`}
+      alt="portfolio image"
+    />
+  );
+};
 
-export default function Home() {
-  return <h1>luna goodwin</h1>;
-}
+const Home = () => {
+  return (
+    <div className="object-cover h-[54.3rem] w-1/2 md:w-2/3 border-r border-white/10 pt-4 pr-4">
+      <DisplayImage name="img2.webp" />
+    </div>
+  );
+};
+
+export default Home;

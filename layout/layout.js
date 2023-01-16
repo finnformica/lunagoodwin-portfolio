794c381e854c43ea9f4347da12f1a62afa403ca5
finnformica <finnformica@gmail.com>
2023-01-16 00:05:37 +0000

    add fade page transition

--- layout/layout.js
+++ layout/layout.js
@@ -1,23 +1,40 @@
-import Link from "next/link";
-import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
 
 import Header from "../components/Header/Header";
 
 const Layout = ({ children }) => {
-  const router = useRouter();
+  const [displayChildren, setDisplayChildren] = useState(children);
+  const [transitionStage, setTransitionStage] = useState("fadeOut");
+
+  // fade in and out page transition
+  useEffect(() => {
+    setTransitionStage("fadeIn");
+  }, []);
+
+  useEffect(() => {
+    if (children !== displayChildren) setTransitionStage("fadeOut");
+  }, [children, setDisplayChildren, displayChildren]);
+
   return (
     <>
       <div className="flex flex-col min-h-screen">
         <Header />
         <main
+          onTransitionEnd={() => {
+            if (transitionStage === "fadeOut") {
+              setDisplayChildren(children);
+              setTransitionStage("fadeIn");
+            }
+          }}
           style={{
             flexGrow: 1,
             display: "flex",
             flexDirection: "column",
+            opacity: transitionStage === "fadeIn" ? 1 : 0,
+            transition: "1s",
           }}
-          // className="bg-red-500 grow"
         >
-          {children}
+          {displayChildren}
         </main>
       </div>
     </>

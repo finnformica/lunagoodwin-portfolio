86aad2ba17eef69e4b601fa71c530a05999e9bf6
finnformica <finnformica@gmail.com>
2023-01-18 22:42:37 +0000

    add SEO optimises

--- pages/_app.js
+++ pages/_app.js
@@ -11,7 +11,7 @@ function MyApp({ Component, pageProps }) {
         <meta name="robots" content="all" />
         <meta
           name="description"
-          content="Portfolio website designer and digital creator Luna Ocean Sunshine."
+          content="Portfolio website for designer and digital creator Luna Ocean Sunshine."
         />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta charSet="utf-8" />
